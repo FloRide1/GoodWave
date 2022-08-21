f57289cf0dd54849c3add0806ab9067263043a3a
FloRide1 <florian.reimat@orange.fr>
2022-08-21 21:49:50 +0200

    [APP] Create new route to /tasks (using Tasks)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Root from "./pages/Root";
+import Tasks from "./pages/Tasks";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Root />} />
+      <Route path="/tasks" element={<Tasks />} />
     </Routes>
   );
 }
