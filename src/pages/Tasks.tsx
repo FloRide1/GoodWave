cb291c366f50c1a628be04abd0f372fe56646d9e
FloRide1 <florian.reimat@orange.fr>
2022-08-23 14:19:52 +0200

    [PAGES] Implement simple creation + deletion of Tasks

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,5 +1,51 @@
+import { Box, List } from "@material-ui/core";
+import Button from "@mui/material/Button";
+import React from "react";
+import { useState } from "react";
+import DragList from "../components/DragList";
+import { TaskList } from "../models/TaskList";
+
+const init_list: Array<TaskList> = [];
+
+// TODO
+init_list.push({
+  name: "TODO",
+  items: [],
+  frontColor: "tomato",
+  backColor: "firebrick",
+});
+
+// IN PROGRESS
+init_list.push({
+  name: "IN PROGRESS",
+  items: [],
+  frontColor: "orange",
+  backColor: "darkorange",
+});
+
+// DONE
+init_list.push({
+  name: "DONE",
+  items: [],
+  frontColor: "green",
+  backColor: "darkgreen",
+});
+
 function Tasks() {
-  return <h1> Basic Tasks </h1>;
+  // Object are passed by reference
+  const [lists, setLists] = React.useState(init_list);
+  const [render, rerender] = useState(false);
+
+  // But i still need to rerender the data from child components
+  const onChange = () => {
+    rerender(!render);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      {lists.map((e: TaskList) => DragList(e, onChange))}
+    </Box>
+  );
 }
 
 export default Tasks;
