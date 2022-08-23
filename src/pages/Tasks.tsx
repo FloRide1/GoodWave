cfc3b92494a5333329796604b5208711bd311878
FloRide1 <florian.reimat@orange.fr>
2022-08-23 15:06:25 +0200

    [PAGES] Implement Drag&Drop (+ function associtated with) on Tasks

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,8 +1,10 @@
 import { Box, List } from "@material-ui/core";
 import Button from "@mui/material/Button";
+import { DragDropContext } from "@react-forked/dnd";
 import React from "react";
 import { useState } from "react";
 import DragList from "../components/DragList";
+import { Task } from "../models/Task";
 import { TaskList } from "../models/TaskList";
 
 const init_list: Array<TaskList> = [];
@@ -36,6 +38,50 @@ function Tasks() {
   const [lists, setLists] = React.useState(init_list);
   const [render, rerender] = useState(false);
 
+  const reorder = (list: Array<any>, startIndex: number, endIndex: number) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  const addToList = (list: Array<Task>, index: number, e: Task) => {
+    const result = Array.from(list);
+    result.splice(index, 0, e);
+    return result;
+  };
+
+  const removeFromList = (list: Array<Task>, index: number) => {
+    const result = Array.from(list);
+    result.splice(index, 1);
+    return result;
+  };
+
+  function onDragEnd(result: any) {
+    const { source, destination } = result;
+
+    if (!destination) return;
+
+    const sInd = +source.droppableId;
+    const dInd = +destination.droppableId;
+
+    // Drop on the same list
+    if (sInd === dInd) {
+      lists[sInd].items = reorder(
+        lists[sInd].items,
+        source.index,
+        destination.index
+      );
+      setLists(lists);
+    } else {
+      const item = lists[sInd].items[source.index];
+      lists[sInd].items = removeFromList(lists[sInd].items, source.index);
+      lists[dInd].items = addToList(lists[dInd].items, destination.index, item);
+      setLists(lists);
+    }
+  }
+
   // But i still need to rerender the data from child components
   const onChange = () => {
     rerender(!render);
@@ -43,7 +89,11 @@ function Tasks() {
 
   return (
     <Box sx={{ display: "flex" }}>
-      {lists.map((e: TaskList) => DragList(e, onChange))}
+      <DragDropContext onDragEnd={onDragEnd}>
+        {lists.map((e: TaskList, index: number) =>
+          DragList(index, e, onChange)
+        )}
+      </DragDropContext>
     </Box>
   );
 }
